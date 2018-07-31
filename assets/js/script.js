2a7961b30fa021d750ccf3964db411839b214b63
hhamza1 <hamzahmitou@gmail.com>
2018-07-31 05:18:56 +0300

    Add the styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,18 @@
-$("li").on("click", function(){
+$("ul").on("click","li", function(){
   $(this).toggleClass("checkedOff");
 });
 
-$("span").click(function(e){
+$("ul").on("click","span",function(e){
   $(this).parent().fadeOut(500, function(){
     $(this).remove();
   });
   e.stopPropagation();
 });
+
+$("input[type='text']").keypress(function(e){
+  if (e.which === 13){
+    var todo = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span>X</span> " + todo + "</li>")
+  }
+})
