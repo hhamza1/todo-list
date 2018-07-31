89c1637a5722d957cb6d88ba121755a582fcb620
hhamza1 <hamzahmitou@gmail.com>
2018-07-31 05:42:56 +0300

    Add font-awesome

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,6 @@ $("input[type='text']").keypress(function(e){
   if (e.which === 13){
     var todo = $(this).val();
     $(this).val("");
-    $("ul").append("<li><span>X</span> " + todo + "</li>")
+    $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todo + "</li>")
   }
 })
